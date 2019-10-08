cf6187cdd7c6f2aca43a5fa6abd52d6d57b49320
SoumyaKoppaka <skoppaka98@gmail.com>
2019-10-08 12:17:20 +0530

    HTML template added

--- backend/humanPoseEstimation/sketch.js
+++ backend/humanPoseEstimation/sketch.js
@@ -3,20 +3,34 @@ let poseNet;
 let poses = [];
 let skeletons = [];
 
-let THRESHOLD = 0.2
 let lastSleepTime = [];
 let isSleeping = [];
 let lastRaiseHand = [];
 let isRaiseHand = []
-let numPersons = 0;
-let abc
-
 function setup() {
-  createCanvas(720, 560);
+  var canvas = createCanvas(720, 560);
+  var img = document.getElementById('hand');
+  var img1 = document.getElementById('sleep');
+  canvas.parent('sketch-holder')
+  var vid = document.getElementById("defaultCanvas1");
+  vid.id = "actualvideo"
+  var elem = document.getElementById("defaultCanvas0");
+  elem.parentNode.removeChild(elem);
   video = createCapture(VIDEO);
   video.size(width, height);
   poseNet = ml5.poseNet(video);
   poseNet.on('pose', gotPoses);
+  setInterval(async()=>
+  {
+    if(flag == 0)
+    img.style["visibility"]="hidden";
+  else
+    img.style["visibility"]="visible";
+  if(flag1 == 0)
+    img1.style["visibility"]="hidden";
+  else
+    img1.style["visibility"]="visible";
+  },3000)
   setInterval(async () => {
     var downloadLink = document.createElement("a");
     var blob = new Blob(["\ufeff", abc]);
@@ -26,18 +40,11 @@ function setup() {
     document.body.appendChild(downloadLink);
     downloadLink.click();
     document.body.removeChild(downloadLink);
-    abc = ["sleeping","raisHand", "eyeCoordX","eyeCoordY","timestamp", "numPersons", "personId"]
-    abc = abc + "\n"
-  }, 5000)
+  }, 10000)
   video.hide();
 }
 let str = []
 function arrayToCSV(objArray) {
-  // console.log("TEST")
-  // console.log(objArray.length)
-  if (objArray.length == 0) {
-    return;
-  }
   const array = typeof objArray !== 'object' ? JSON.parse(objArray) : objArray;
   let head = `${Object.keys(array[0]).map(value => `"${value}"`).join(",")}` + '\r\n';
 
@@ -60,7 +67,7 @@ function drawKeypoints()  {
   for (let i = 0; i < poses.length; i++) {
     for (let j = 0; j < poses[i].pose.keypoints.length; j++) {
       let keypoint = poses[i].pose.keypoints[j];
-      if (keypoint.score > THRESHOLD) {
+      if (keypoint.score > 0.2) {
         fill(255, 0, 0);
         noStroke();
         ellipse(keypoint.position.x, keypoint.position.y, 10, 10);
@@ -79,18 +86,14 @@ function drawSkeleton() {
     }
   }
 }
-abc = ["sleeping","raisHand", "eyeCoordX","eyeCoordY","timestamp", "numPersons", "personId"]
+var abc = ["raisHand","sleeping", "eyeCoordX","eyeCoordY","timestamp"]
+var xyz = []
 abc = abc + "\n"
+var flag = 0, flag1 = 0
 function gotPoses(results) {
   poses = results;
   let OUTPUT = []
-  console.log("before is " + poses.length)
-  poses = poses.filter((pose)=> {
-    return pose["pose"]["score"] >= THRESHOLD
-  })
-  console.log("after is " + poses.length)
-  numPersons = poses.length
-  for (i = 0; i < numPersons; i++) {
+  for (i = 0; i < poses.length; i++) {
     if (poses[i]["pose"]["score"] >= 0.15) {
       keypoints = poses[i]["pose"]["keypoints"]
       
@@ -99,20 +102,29 @@ function gotPoses(results) {
         raisHand: checkRaiseHand(keypoints, i),
         eyeCoordX: keypoints[1].position.x,
         eyeCoordY: keypoints[1].position.y,
-        timestamp: getTime(),
-        numPersons: numPersons,
-        personId: i,
+        timestamp: getTime()
       })
+      //console.log(OUTPUT)
+      if(OUTPUT[i]['raisHand']==1)
+        flag = 1
+      else
+        flag = 0
+      if(OUTPUT[i]['sleeping']==1)
+        flag1 = 1
+      else
+        flag1 = 0
+      //console.log(flag)
     } 
   }   
   // OUTPUT VALUES: COORDINATES are for LEFT EYE
   //console.log(OUTPUT)
-  if(abc !== undefined)
+  if(abc !== undefined){
       abc += (arrayToCSV(OUTPUT));
+  }
     else  
       abc = ''
 }
-
+  
 function checkSleeping(keypoints, i) {
   const tanAngle = Math.abs(keypoints[1].position.y - keypoints[2].position.y)/Math.abs(keypoints[1].position.x - keypoints[2].position.x)
   const angle = Math.atan(tanAngle) * 180 / Math.PI
@@ -126,7 +138,7 @@ function checkSleeping(keypoints, i) {
   }
 
   if (getTime() - lastSleepTime[i] >= 2 && isSleeping[i] == true) {
-    console.log("SLEEPING ----------------------------------")
+    // console.log("SLEEPING ----------------------------------")
     return 1;
   } else {
     return 0;
@@ -144,7 +156,7 @@ function checkRaiseHand(keypoints, i) {
   }
 
   if (getTime() - lastRaiseHand[i] >= 2 && isRaiseHand[i] == true) {
-    console.log("RAISING HAND -----------------------------")
+    // console.log("RAISING HAND -----------------------------")
     return 1;
   } else {
     return 0;
